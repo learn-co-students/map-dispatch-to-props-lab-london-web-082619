c8678bc2b8a63a5696fa8f553e8674085b2ca7f0
Abdullah Bajaman <ambajaman@gmail.com>
2019-12-01 02:32:28 +0000

    Done.

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -24,6 +24,7 @@ export class RestaurantInput extends Component {
   handleOnSubmit = event => {
     event.preventDefault();
     // add missing code
+    // addRestaurant is a props comes from mapDispatchToProps, its takes an argument to change the restauran state
     this.props.addRestaurant(this.state)
   }
 
@@ -55,11 +56,11 @@ const mapStateToProps = (state) =>{
 }
 
 const mapDispatchToProps = dispatch => {
-  // restaurant : is a state, comes from mapStateToProps. 
+  // restaurant : is an argument, comes from handleSubmit. 
   // we pass it as an argument to the action in dispatch. 
   // the action.type is ADD_RESTAURANT
   return { addRestaurant: (restaurant) =>{
-    debugger
+
     dispatch(addRestaurant(restaurant))
   }}
 }
